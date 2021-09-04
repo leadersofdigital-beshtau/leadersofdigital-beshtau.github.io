1228108049bed2a44fb4582f4a5a7a01dd1dc93f
Artem Sokolov <artemsokolov@hotmail.com>
2021-09-04 05:58:52 +0300

    Обновление

--- js/about.ecff5353.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,a,s){"use strict";s.r(a);var e=s("7a23"),n={class:"about"},c=Object(e["g"])("h1",{class:"about__title"},"This is an about page",-1),o=[c];function u(t,a){return Object(e["s"])(),Object(e["f"])("div",n,o)}const b={};b.render=u;a["default"]=b}}]);
-//# sourceMappingURL=about.ecff5353.js.map
